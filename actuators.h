5ea6d8f153e6d17762465cff8cc8395947be48b1
Richard Franks <git@rf152.co.uk>
2024-12-01 10:21:49 +0000

    Ran the tools to generate the headers

---
+++ actuators.h
@@ -0,0 +1,18 @@
+
+/* Copyright (C) 2024 Richard Franks - All Rights Reserved
+ *
+ * You may use, distribute and modify this code under the
+ * terms of the Apache 2.0 license.
+ *
+ * See LICENSE for details
+ */
+
+#pragma once
+#define LIGHT_HEADLIGHT           0x01  // Size: 1 byte
+#define LIGHT_SIDELIGHTS          0x02  // Size: 1 byte
+#define LIGHT_CAB_LIGHT           0x03  // Size: 1 byte
+#define LIGHT_TAIL_LIGHT          0x04  // Size: 1 byte
+
+#define MOTOR_WINDSCREEN_WIPERS   0x40  // Size: 1 byte
+
+#define POT_THROTTLE              0x80  // Size: 1 byte
---
+++ config.h
@@ -0,0 +1,13 @@
+
+/* Copyright (C) 2024 Richard Franks - All Rights Reserved
+ *
+ * You may use, distribute and modify this code under the
+ * terms of the Apache 2.0 license.
+ *
+ * See LICENSE for details
+ */
+
+#pragma once
+
+#define NODE_ID   0x0000  // Size: 1 byte
+#define STOCK     0x0001  // Size: 1 byte
