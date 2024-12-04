871b3108a21af57452fc8b22c3076a17f203692f
Richard Franks <git@rf152.co.uk>
2024-12-04 15:50:31 +0000

    Updated masks for matching packets

--- masks.h
+++ masks.h
@@ -8,27 +8,28 @@
 
 #pragma once
 
-#define MASK_FUNC                     0x0700
+#define MASK_FUNC               0x0700
 
-#define MATCH_FUNC_EMCY               0x0100
-#define MATCH_FUNC_SENSOR             0x0200
-#define MATCH_FUNC_ACTUATOR           0x0300
-#define MATCH_FUNC_CONFIG_TX          0x0400
-#define MATCH_FUNC_CONFIG_RX          0x0500
-#define MATCH_FUNC_HBEAT              0x0700
+#define FUNC_EMCY               0x0100
+#define FUNC_SENSOR             0x0200
+#define FUNC_ACTUATOR           0x0300
+#define FUNC_CONFIG_TX          0x0400
+#define FUNC_CONFIG_RX          0x0500
+#define FUNC_HBEAT              0x0700
 
-#define MASK_STOCK                    0x00C0
+#define MASK_STOCK              0x00C0
 
-#define MATCH_STOCK_LOCO1             0x0000
-#define MATCH_STOCK_LOCO2             0x0040
-#define MATCH_STOCK_CARRIAGE          0x0080
-#define MATCH_STOCK_BROADCAST         0x00C0
+#define STOCK_LOCO1             0x0000
+#define STOCK_LOCO2             0x0040
+#define STOCK_CARRIAGE          0x0080
+#define STOCK_BROADCAST         0x00C0
 
-#define MASK_NODE                     0x003F
+#define MASK_NODE               0x003F
 
-#define MATCH_NODE_CORE_CONTROLLERS   0x0000
-#define MATCH_NODE_BATTERY            0x0004
-#define MATCH_NODE_LOCO               0x0008
-#define MATCH_NODE_SENSORS            0x0010
-#define MATCH_NODE_CONTROLLERS        0x0020
-#define MATCH_NODE_PROGRAMMING        0x003F
+#define NODE_CORE_CONTROLLERS   0x0000
+#define NODE_BATTERY            0x0004
+#define NODE_LOCO               0x0008
+#define NODE_SENSORS            0x0010
+#define NODE_CONTROLLERS        0x0020
+#define NODE_BROADCAST          0x003E
+#define NODE_PROGRAMMING        0x003F
